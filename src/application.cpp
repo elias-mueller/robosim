d3a20edeb2e50343c521183f947ab825b864e1b7
Elias Mueller <elias.mueller7@gmail.com>
2019-08-15 20:28:51 +0200

    refac: Remove auto that hides types

--- src/application.cpp
+++ src/application.cpp
@@ -7,8 +7,8 @@ void Application::setup() {
   ApplicationContextBase::setup();
   addInputListener(this);
 
-  auto root = getRoot();
-  auto scn_mgr = root->createSceneManager();
+  Root *root = getRoot();
+  SceneManager *scn_mgr = root->createSceneManager();
 
   auto shader_gen = RTShader::ShaderGenerator::getSingletonPtr();
   shader_gen->addSceneManager(scn_mgr);
@@ -23,23 +23,23 @@ void Application::setup_light(SceneManager &scn_mgr) const {
   scn_mgr.setAmbientLight(ColourValue(0.5, 0.5, 0.5));
   scn_mgr.setShadowTechnique(ShadowTechnique::SHADOWTYPE_TEXTURE_MODULATIVE);
 
-  auto light = scn_mgr.createLight("MainLight");
-  auto light_node = scn_mgr.getRootSceneNode()->createChildSceneNode();
+  Light *light = scn_mgr.createLight("MainLight");
+  SceneNode *light_node = scn_mgr.getRootSceneNode()->createChildSceneNode();
   light_node->attachObject(light);
 
   light_node->setPosition(20, 80, 50);
 }
 
 void Application::setup_camera(SceneManager &scn_mgr) const {
-  auto cam_node = scn_mgr.getRootSceneNode()->createChildSceneNode();
+  SceneNode *cam_node = scn_mgr.getRootSceneNode()->createChildSceneNode();
 
-  auto cam = scn_mgr.createCamera("myCam");
+  Camera *cam = scn_mgr.createCamera("myCam");
   cam->setNearClipDistance(5); // specific to this sample
   cam->setAutoAspectRatio(true);
   cam_node->attachObject(cam);
   cam_node->setPosition(0, 47, 222);
 
-  auto vp = getRenderWindow()->addViewport(cam);
+  Viewport *vp = getRenderWindow()->addViewport(cam);
   vp->setBackgroundColour(ColourValue::White);
 }
 
@@ -48,16 +48,16 @@ void Application::create_plane(Ogre::SceneManager &scn_mgr) const {
   MeshManager::getSingleton().createPlane("ground", RGN_DEFAULT, plane, 1500,
                                           1500, 20, 20, true, 1, 5, 5,
                                           Vector3::UNIT_Z);
-  auto entity = scn_mgr.createEntity("ground");
+  Entity *entity = scn_mgr.createEntity("ground");
   entity->setCastShadows(false); // No need for shadows from the ground.
   entity->setMaterialName("Examples/Rockwall");
 
-  auto node = scn_mgr.getRootSceneNode()->createChildSceneNode();
+  SceneNode *node = scn_mgr.getRootSceneNode()->createChildSceneNode();
   node->attachObject(entity);
 }
 
 void Application::create_entities(SceneManager &scn_mgr) const {
-  auto node = create_entity(scn_mgr, "ogrehead.mesh");
+  SceneNode *node = create_entity(scn_mgr, "ogrehead.mesh");
   node->setPosition(Vector3(-84, 48, 0));
 
   node = create_entity(scn_mgr, "ogrehead.mesh");
@@ -70,8 +70,8 @@ void Application::create_entities(SceneManager &scn_mgr) const {
 
 SceneNode *Application::create_entity(SceneManager &scn_mgr,
                                       const std::string &mesh_name) const {
-  auto entity = scn_mgr.createEntity(mesh_name);
-  auto node = scn_mgr.getRootSceneNode()->createChildSceneNode();
+  Entity *entity = scn_mgr.createEntity(mesh_name);
+  SceneNode *node = scn_mgr.getRootSceneNode()->createChildSceneNode();
   node->attachObject(entity);
 
   return node;
